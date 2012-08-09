7c8dfe324d446d404e867ccc7d03d3978c7ff523
Evan Gamble <egamble@factual.com>
2012-08-08 18:46:35 -0700

    C-c C-SPC for ace-jump-mode

--- init.el
+++ init.el
@@ -350,3 +350,4 @@ Leave one space or none, according to the context."
 
 (require 'ace-jump-mode)
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-c C-SPC") 'ace-jump-mode)
