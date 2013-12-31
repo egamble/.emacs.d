1a319c2823caa6db3a8d0a58938da68924aa14c9
Evan Gamble <egamble@factual.com>
2013-12-30 17:27:02 -0800

    remove special syntax coloring for double quotes

--- init.el
+++ init.el
@@ -192,6 +192,7 @@
             (local-set-key (kbd "<C-M-s-left>") 'backward-select-sexp)
             )))
 
+
 (defmacro defclojureface (name color desc &optional others)
   `(defface ,name '((((class color)) (:foreground ,color ,@others))) ,desc :group 'faces))
 
@@ -200,21 +201,17 @@
 (defclojureface clojure-braces       "#49b2c7"   "Clojure braces")
 (defclojureface clojure-brackets     "SteelBlue" "Clojure brackets")
 (defclojureface clojure-keyword      "khaki"     "Clojure keywords")
-(defclojureface clojure-namespace    "#c476f1"   "Clojure namespace")
-(defclojureface clojure-java-call    "#4bcf68"   "Clojure Java calls")
 (defclojureface clojure-special      "#b8bb00"   "Clojure special")
 
-(defun tweak-clojure-syntax ()
+(defun tweak-clojure-coloring ()
   (mapcar (lambda (x) (font-lock-add-keywords nil x))
           '((("#?['`]*(\\|)"       . 'clojure-parens))
             (("#?\\^?{\\|}"        . 'clojure-brackets))
             (("\\[\\|\\]"          . 'clojure-braces))
             ((":\\w+"              . 'clojure-keyword))
-            (("#?\""               0 'clojure-special prepend))
-            (("nil\\|true\\|false\\|%[1-9]?" . 'clojure-special))
-            (("(\\(\\.[^ \n)]*\\|[^ \n)]+\\.\\|new\\)\\([ )\n]\\|$\\)" 1 'clojure-java-call)))))
+            (("nil\\|true\\|false\\|%[1-9]?" . 'clojure-special)))))
 
-(add-hook 'clojure-mode-hook 'tweak-clojure-syntax)
+(add-hook 'clojure-mode-hook 'tweak-clojure-coloring)
 
 
 (defun smart-line-beginning ()
