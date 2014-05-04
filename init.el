0e19da56888024e5d0a93e0ef32430ecb9fd3c85
Evan Gamble <egamble@factual.com>
2014-05-04 16:09:15 -0700

    cider-pprint-defun-at-point-in-place preserves comments

--- init.el
+++ init.el
@@ -659,7 +659,7 @@ Leave one space or none, according to the context."
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-  '(default ((t (:height 150)))))
+  '(default ((t (:height 140)))))
 
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
