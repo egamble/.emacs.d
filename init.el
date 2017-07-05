db874539e7299f2839eb143e80bc8d1c8551a8a9
Evan Gamble <solar.fuel@gmail.com>
2017-07-05 14:46:10 -0700

    revert c-basic-offset to 2

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
 ;; tab width as two, using spaces
 (setq default-tab-width 2)
 (setq-default indent-tabs-mode nil)
-(setq c-basic-offset 4)
+;; (setq c-basic-offset 4)
 
 ;; show column numbers
 (setq column-number-mode t)
