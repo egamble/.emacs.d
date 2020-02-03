d71d6bb50ba2b29ad2e8baa8b767b589f6421a6f
Evan Gamble <evan.gamble@freiheit.com>
2020-02-03 16:36:37 +0100

    fira code for typescript

--- lang-inits/other-langs.el
+++ lang-inits/other-langs.el
@@ -72,7 +72,7 @@ Modified from sanityinc's answer to http://stackoverflow.com/questions/8606954/p
 (add-hook 'typescript-mode-hook 'linum-mode)
 
 
-(dolist (mode '(haskell markdown python go))
+(dolist (mode '(haskell markdown python go typescript))
   (add-hook (first (read-from-string (concat (symbol-name mode) "-mode-hook")))
             (lambda ()
               (when auto-fira-code-mode
